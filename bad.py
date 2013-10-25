513560a051d9388cd39384860ddce6a938501080
Brint O'Hearn <brint.ohearn@rackspace.com>
2013-10-25 16:48:07 -0500

    Allow user to set their initial amount of cash and drugs

--- bad.py
+++ bad.py
@@ -5,12 +5,20 @@ import time
 driver = webdriver.Chrome()
 driver.get("http://clickingbad.nullism.com/")
 
-num_cooks = 100
-num_sells = 50
+# Amount you'd like to have in terms of cash and
+# drugs to start the game
+init_drugs = 10000
+init_cash = 10000
+
+# Number of cooks and sells to do in a row
+num_cooks = 500
+num_sells = 500
 
 cook = driver.find_element_by_id('make_btn')
 sell = driver.find_element_by_id('sell_btn')
 
+driver.execute_script("gm.add_widgets(" + str(init_drugs) + ")")
+driver.execute_script("gm.add_cash(" + str(init_cash) + ")")
 while True:
     try:
         counter = 0
@@ -18,7 +26,6 @@ while True:
         while counter < num_cooks:
             cook.click()
             counter+=1
-        time.sleep( 1 )
         counter = 0
         driver.execute_script("window.scrollTo(0,0);")
         while counter < num_sells:
